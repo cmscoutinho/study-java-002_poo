088adf6fa18bc9a7b5de00d8f52a03eb82c4631c
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-05-26 17:54:54 -0300

    01_09: code for exercise 2

---
+++ screenmatch/src/ex_01_09/ex_01/Calculadora.java
@@ -0,0 +1,13 @@
+package ex_01_09.ex_01;
+
+public class Calculadora {
+    public int dobro(int numero) {
+        return 2*numero;
+    }
+
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        System.out.println(calculadora.dobro(14));
+        System.out.println(calculadora.dobro(56));
+    }
+}
