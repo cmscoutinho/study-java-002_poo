3d5ff5f82cb18d05bf55f2c69f699374358b1dda
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-05-26 17:53:38 -0300

    01_09: code for exercise 1

---
+++ screenmatch/src/ex_01_09/ex_01/Pessoa.java
@@ -0,0 +1,12 @@
+package ex_01_09.ex_01;
+
+public class Pessoa {
+    public void olaMundo() {
+        System.out.println("Olá, mundo!");
+    }
+
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        pessoa.olaMundo();
+    }
+}
