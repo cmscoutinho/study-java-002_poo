50ce2353e6dccf78197c581e00c0c82ada77153d
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-05-30 15:21:27 -0300

    05_04: create, play and like instances of subclasses

---
+++ desafio/src/br/com/alura/desafio/main/Main.java
@@ -0,0 +1,32 @@
+package br.com.alura.desafio.main;
+
+import br.com.alura.desafio.model.Musica;
+import br.com.alura.desafio.model.Podcast;
+
+public class Main {
+    public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Forever");
+        minhaMusica.setArtista("Kiss");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduzir();
+        }
+
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curtir();
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("BolhaDev");
+        meuPodcast.setAnfitriao("Marcus Mendes");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduzir();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curtir();
+        }
+    }
+}
