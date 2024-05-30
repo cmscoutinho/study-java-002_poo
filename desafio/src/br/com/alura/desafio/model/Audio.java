c6557540516188d0f00896a5c4c24d3ec72002f4
Claudio Coutinho <ccoutinho.ti@gmail.com>
2024-05-30 15:12:13 -0300

    05_03: add fields of subclasses

--- desafio/src/br/com/alura/desafio/model/Audio.java
+++ desafio/src/br/com/alura/desafio/model/Audio.java
@@ -2,7 +2,6 @@ package br.com.alura.desafio.model;
 
 public class Audio {
     private String titulo;
-    private int duracao;
     private int totalReproducoes;
     private int curtidas;
     private int classificacao;
@@ -15,43 +14,23 @@ public class Audio {
         this.titulo = titulo;
     }
 
-    public int getDuracao() {
-        return duracao;
-    }
-
-    public void setDuracao(int duracao) {
-        this.duracao = duracao;
-    }
-
     public int getTotalReproducoes() {
         return totalReproducoes;
-    }
-
-    public void setTotalReproducoes(int totalReproducoes) {
-        this.totalReproducoes = totalReproducoes;
     }
 
     public int getCurtidas() {
         return curtidas;
     }
 
-    public void setCurtidas(int curtidas) {
-        this.curtidas = curtidas;
-    }
-
     public int getClassificacao() {
         return classificacao;
     }
 
-    public void setClassificacao(int classificacao) {
-        this.classificacao = classificacao;
-    }
-
     public void curtir() {
-
+        curtidas++;
     }
 
     public void reproduzir() {
-
+        totalReproducoes++;
     }
 }
--- desafio/src/br/com/alura/desafio/model/Podcast.java
+++ desafio/src/br/com/alura/desafio/model/Podcast.java
@@ -1,4 +1,22 @@
 package br.com.alura.desafio.model;
 
 public class Podcast extends Audio {
+    private String anfitriao;
+    private String descricao;
+
+    public String getAnfitriao() {
+        return anfitriao;
+    }
+
+    public void setAnfitriao(String anfitriao) {
+        this.anfitriao = anfitriao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
